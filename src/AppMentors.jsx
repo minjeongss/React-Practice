e9e77aed491d3edbdaf2909c2db9a7f0c7625c9a
Kim Min Jeong <dotoriido@gmail.com>
2023-07-29 18:31:39 +0900

    Update for cleancode

--- src/AppMentors.jsx
+++ src/AppMentors.jsx
@@ -1,19 +1,31 @@
 import React, { useState } from 'react';
 
 export default function AppMentors() {
-    const [person,setPerson]=useState({
-        name:'minjeong',
-        title:'developer',
-        mentors:[{
-            id:1,
-            name:'elly',
-            title:'senior developer'
-        },{
-            id:2,
-            name:'hoho',
-            title:'developer'
-        }
-    ]})
+    const [person,setPerson]=useState(initialPerson);
+    const handleUpdate=()=>{
+        const target=prompt(`바꿀 사람의 이름은 무엇입니까?`);
+                const current=prompt(`바꾸고자 하는 사람의 이름은 무엇입니까?`);
+                setPerson((prev)=>({...prev,mentors:person.mentors.map((mentor=>{
+                    if (target===mentor.name){
+                        return {...mentor,name:current};
+                    }
+                    return mentor;
+                }))}));
+    };
+    const handleDelete=()=>{
+        const target=prompt(`삭제할 사람의 이름은 무엇입니까?`);
+                setPerson((prev)=>({...prev,
+                    mentors:person.mentors.filter((mentor=>mentor.name!==target
+                ))}));
+    };
+    const handleAdd=()=>{
+        const target=prompt(`추가할 사람의 이름은 무엇입니까?`);
+                const title=prompt(`추가할 사람의 직함은 무엇입니까?`);
+                console.log(target,title);
+                setPerson((prev)=>({...prev,
+                    mentors:[...prev.mentors,{target,title}],
+                    }));
+    };
     return (
         <div>
             <h1>
@@ -27,33 +39,23 @@ export default function AppMentors() {
                     )))}
                 </ul>
             </p>
-            <button onClick={()=>{
-                const target=prompt(`바꿀 사람의 이름은 무엇입니까?`);
-                const current=prompt(`바꾸고자 하는 사람의 이름은 무엇입니까?`);
-                setPerson((prev)=>({...prev,mentors:person.mentors.map((mentor=>{
-                    if (target===mentor.name){
-                        return {...mentor,name:current};
-                    }
-                    return mentor;
-                }))}));
-            }}>멘토 이름 바꾸기</button>
-            
-            <button onClick={()=>{
-                const target=prompt(`삭제할 사람의 이름은 무엇입니까?`);
-                setPerson((prev)=>({...prev,
-                    mentors:person.mentors.filter((mentor=>mentor.name!==target
-                ))}));
-            }}>멘토 삭제하기</button>
-
-            <button onClick={()=>{
-                const target=prompt(`추가할 사람의 이름은 무엇입니까?`);
-                const title=prompt(`추가할 사람의 직함은 무엇입니까?`);
-                console.log(target,title);
-                setPerson((prev)=>({...prev,
-                    mentors:[...prev.mentors,{target,title}],
-                    }));
-            }}>멘토 추가하기</button>
+            <button onClick={handleUpdate}>멘토 이름 바꾸기</button>
+            <button onClick={handleDelete}>멘토 삭제하기</button>
+            <button onClick={handleAdd}>멘토 추가하기</button>
         </div>
     );
 }
 
+const initialPerson={
+    name:'minjeong',
+    title:'developer',
+    mentors:[{
+        id:1,
+        name:'elly',
+        title:'senior developer'
+    },{
+        id:2,
+        name:'hoho',
+        title:'developer'
+    }
+]}
